bec8bf13348e2ffeb2471f8e554e6590fd753199
Khalishah18 <khalishahsalsabilla18@gmail.com>
2023-11-16 13:50:33 +0700

    Menambahkan fun FormatDataPemesan di FormatUI

--- app/src/main/java/com/example/esjumbo/ui/komponen/FormatUI.kt
+++ app/src/main/java/com/example/esjumbo/ui/komponen/FormatUI.kt
@@ -1,10 +1,14 @@
 package com.example.esjumbo.ui.komponen
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.esjumbo.R
 
 @Composable
@@ -14,4 +18,28 @@ fun FormatLabelHarga(subtotal: String, modifier: Modifier=Modifier){
         modifier = modifier,
         style = MaterialTheme.typography.headlineSmall
     )
+}
+@Composable
+fun FormatDataPemesan(
+    namaPemesan: String,
+    alamatPemesan: String,
+    phonePemesan: String,
+    ){
+    Text(
+        text = stringResource(R.string.namaOrder),
+        )
+    Text(namaPemesan)
+    Divider()
+    Spacer(modifier = Modifier.padding(8.dp))
+
+    Text(text = stringResource(R.string.alamatOrder),
+        )
+    Text(alamatPemesan)
+    Divider()
+    Spacer(modifier = Modifier.padding(10.dp))
+
+    Text(text = stringResource(R.string.phoneOrder),
+        )
+    Text(phonePemesan)
+    Divider()
 }
