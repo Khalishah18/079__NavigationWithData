2c577ad2e8131ef96979964d5431b69e6b4d90ee
Khalishah18 <khalishahsalsabilla18@gmail.com>
2023-11-16 12:29:50 +0700

    Menambahkan Row dihalaman form

--- app/src/main/java/com/example/esjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -2,10 +2,12 @@ package com.example.esjumbo
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -73,6 +75,17 @@ fun HalamanForm(
                 onValueChange = {phoneTxt = it}
             )
             Spacer(modifier = Modifier.padding(10.dp))
+        }
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ){
+            Button(onClick = {onBackButtonClicked}) {
+                Text(text = stringResource(id = R.string.back_button))
+            }
+            Button(onClick = {onSubmitButtonClicked}) {
+                Text(text = stringResource(id = R.string.submit_button))
+            }
         }
     }
 }
